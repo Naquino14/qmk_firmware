aba6800bbd8bc2e50143586a6ad066f677983cb6
Nate Aquino <naquino14isaboss@gmail.com>
2025-07-11 20:42:26 -0400

    Add LED Matrix support

--- keyboards/nd68/config.h
+++ keyboards/nd68/config.h
@@ -2,3 +2,11 @@
 #define ENCODER_B_PINS { GP22 }
 #define ENCODER_RESOLUTION 4
 #define ENCODERS_BUTTON_GPIO GP24
+
+// Led Matrix Defs
+#define WS2812_DI_PIN GP21
+#define RGB_MATRIX_LED_COUNT 70 // 68 keys + 2 more for spacebar
+#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 190 // rev 1 missing decouplers, this is a safety net
+
+// RGB effects
+#define ENABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
